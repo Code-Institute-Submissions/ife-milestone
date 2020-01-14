02caff64fd07583098e7990b00485bdf26916476
Amelia Corcoran <milliecorcoran@virginmedia.com>
2020-01-14 16:47:58 +0000

    Images added to cards (pre flip), bugs fixed

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ $(document).ready(function () {
     let card = document.getElementsByClassName('game-card');
     let stopwatch = document.getElementsByClassName('timer');
 
-    var movesTaken = 0;
-    var seconds = 0;
-    var minutes = 0;
+    let movesTaken;
+    let seconds = 0;
+    let minutes = 0;
     const unmatchSound = document.getElementById('wrong');
     const matchSound = document.getElementById('correct');
 
@@ -21,13 +21,14 @@ $(document).ready(function () {
         $('.move-counter').text('Moves: ' + 0);
         seconds = 0;
         minutes = 0;
-        $('div').removeClass('flipped matched no-click');
+        $('div').removeClass('flipped matched');
         shuffleDeck();
         chooseCards();
         checkMatch();
         clearInterval(timeTaken);
         $('.timer').text('Timer: 00:00');
         timer();
+        enableClicks();
     }
 
     function reset() {
@@ -41,15 +42,16 @@ $(document).ready(function () {
         $('div').removeClass('flipped matched');
         clearInterval(timeTaken);
         $('.timer').text('Timer: 00:00');
+        $(".pokemon").addClass("hidden");
+        $(".pokeball").removeClass("hidden");
         $('#reset-message').removeClass('transparent').addClass('opaque animated lightSpeedIn');
         setTimeout(function () {
             $('#reset-message').removeClass('lightSpeedIn').addClass('lightSpeedOut')
         }, 3000);
         setTimeout(function () {
             $('#reset-message').removeClass('lightSpeedOut opaque').addClass('transparent')
-        }, 4000);
-        
-    }
+        }, 4000);   
+    };
 
     function startGame() {
         $('#start').click(function () {
@@ -60,20 +62,22 @@ $(document).ready(function () {
     function resetGame() {
         $('#reset').click(function () {
             reset();
-        })
-    }
+        });
+    };
 
     function shuffleDeck() {
-        var parent = $("#game");
-        var divs = parent.children();
+        let parent = $("#game");
+        let divs = parent.children();
         while (divs.length) {
             parent.append(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
-        }
+        };
     };
 
     function chooseCards() {
         $(".game-card").click(function () {
             $(this).addClass("flipped");
+            $(this).children(".pokeball").addClass("hidden");
+            $(this).children(".pokemon").removeClass("hidden");
             checkMatch();
 
         });
@@ -81,20 +85,14 @@ $(document).ready(function () {
 
     function checkMatch() {
         if ($(".flipped").length === 2) {
-
             if ($(".flipped").first().data("cardNumber") == $(".flipped").last().data("cardNumber")) {
                 cardMatch();
-
-
-
             } else {
                 cardUnmatch();
-
             };
             setTimeout(function () {
                 gameComplete()
             }, 1300);
-
         };
     };
 
@@ -106,10 +104,11 @@ $(document).ready(function () {
         setTimeout(function () {
             $('.flipped').each(function () {
                 $(this).addClass('matched').removeClass('animated bounceOut flipped');
-            })
-            enableClicks()
+                $(this).children("img").toggleClass('hidden');
+            });
+            enableClicks();
         }, 1100);
-    }
+    };
 
     function cardUnmatch() {
         disableClicks();
@@ -119,15 +118,16 @@ $(document).ready(function () {
         });
         setTimeout(function () {
             $('.flipped').each(function () {
-                $(this).removeClass('animate shake flipped');
+                $(this).removeClass('animated shake flipped');
+                $(this).children("img").toggleClass('hidden');
             })
             enableClicks();
         }, 1100);
-    }
+    };
 
     function enableClicks() {
         $(".game-card").removeClass("no-click");
-    }
+    };
 
     function easy() {
         $("#easy").click(function () {
@@ -138,6 +138,8 @@ $(document).ready(function () {
             clearTimeout(timeTaken);
             $('.timer').text('Timer: 00:00');
             $('.move-counter').text('Moves:' + 0);
+            $(".pokemon").addClass("hidden");
+            $(".pokeball").removeClass("hidden");
         });
     };
 
@@ -147,17 +149,19 @@ $(document).ready(function () {
             disableClicks();
             $('#game').removeClass('hard');
             $("div").removeClass('flipped matched').remove("#card9, #card10, #card11, #card12, #card13, #card14, #card15, #card16, #card17, #card18");
-            var $newdiv;
-            for (var i = 0; i < 4; i++) {
-                var j = i + 9;
-                var k = Math.ceil((j) / 2);
-                $newdiv = $('<div class="game-card" data-card-number=' + k + ' id= card' + (j) + '> <img src="assets/images/' + k + '.png"></img></div>');
+            let $newdiv;
+            for (let i = 0; i < 4; i++) {
+                let j = i + 9;
+                let k = Math.ceil((j) / 2);
+                $newdiv = $('<div class="game-card" data-card-number=' + k + ' id= card' + (j) + '> <img src="assets/images/Pokeball_icon.png" alt="" class="pokeball"> <img src="assets/images/' + k + '.png" class="pokemon"></img></div>');
                 $('#game').append($newdiv);
             }
             clearInterval(timeTaken);
             clearTimeout(timeTaken);
             $('.timer').text('Timer: 00:00');
             $('.move-counter').text('Moves:' + 0);
+            $(".pokemon").addClass("hidden");
+            $(".pokeball").removeClass("hidden");
         });
     };
 
@@ -165,11 +169,11 @@ $(document).ready(function () {
         $("#hard").click(function () {
             disableClicks();
             $("div").removeClass('flipped matched').remove("#card9, #card10, #card11, #card12, #card13, #card14, #card15, #card16, #card17, #card18");
-            var $newdiv;
-            for (var i = 0; i < 10; i++) {
-                var j = i + 9;
-                var k = Math.ceil((j) / 2);
-                $newdiv = $('<div class="game-card" data-card-number=' + k + ' id= card' + (j) + '> <img src="assets/images/' + k + '.png"></img></div>');
+            let $newdiv;
+            for (let i = 0; i < 10; i++) {
+                let j = i + 9;
+                let k = Math.ceil((j) / 2);
+                $newdiv = $('<div class="game-card" data-card-number=' + k + ' id= card' + (j) + '> <img src="assets/images/Pokeball_icon.png" alt="" class="pokeball"> <img src="assets/images/' + k + '.png" class="pokemon"></img></div>');
                 $('#game').append($newdiv);
             }
             $('#game').addClass('hard');
@@ -177,6 +181,8 @@ $(document).ready(function () {
             clearTimeout(timeTaken);
             $('.timer').text('Timer: 00:00');
             $('.move-counter').text('Moves:' + 0);
+            $(".pokemon").addClass("hidden");
+            $(".pokeball").removeClass("hidden");
         });
     };
 
@@ -187,9 +193,9 @@ $(document).ready(function () {
             disableClicks();
             matchSound.play();
             $('#congratulations').modal('show');
-            var finalMinutes = minutes;
-            var finalSeconds = seconds;
-            var finalMoves = movesTaken;
+            let finalMinutes = minutes;
+            let finalSeconds = seconds;
+            let finalMoves = movesTaken;
             $('.total-minutes').text(finalMinutes);
             $('.total-seconds').text(finalSeconds);
             $('.total-moves').text(finalMoves);
@@ -197,7 +203,7 @@ $(document).ready(function () {
         };
     };
 
-    var timeTaken;
+    let timeTaken;
 
     function moveCounter() {
         $('.game-card').click(function () {
